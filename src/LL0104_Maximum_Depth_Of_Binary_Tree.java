737db9b932e96b96131f32af34e4b25891a0454b
wangzhao <195335399@qq.com>
2020-05-12 18:29:16 +0800

    增加DFS深度优先遍历及递归2种实现方式

--- src/LL0104_Maximum_Depth_Of_Binary_Tree.java
+++ src/LL0104_Maximum_Depth_Of_Binary_Tree.java
@@ -1,8 +1,12 @@
 import base.TreeNode;
+import javafx.util.Pair;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
+/**
+ * 二叉树的最大深度
+ */
 public class LL0104_Maximum_Depth_Of_Binary_Tree
 {
     //BSF 广度优先遍历+队列 FIFO
@@ -32,5 +36,49 @@ public class LL0104_Maximum_Depth_Of_Binary_Tree
             }
         }
         return depth;
+    }
+
+    //递归
+    //maxDepth(root) = 1 + maxDepth(root.left) + maxDepth(root.right)
+    public int maxDepth1(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int left = maxDepth1(root.left);
+        int right = maxDepth1(root.right);
+        return 1+Math.max(left,right);
+    }
+
+    //DFS 深度优先遍历
+    public int maxDepth2(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int depth = 1;
+        Stack<Pair<TreeNode,Integer>> stack = new Stack<>();
+        stack.add(new Pair(root,1));
+        while (stack.size()>0)
+        {
+            Pair<TreeNode,Integer> pair = stack.pop();
+            TreeNode pop = pair.getKey();
+            int tmp= pair.getValue();
+            if(pop.right != null)
+            {
+                stack.push(new Pair(pop.right,tmp+1));
+                depth = Math.max(depth,tmp+1);
+            }
+            if(pop.left != null)
+            {
+                stack.push(new Pair(pop.left,tmp+1));
+                depth = Math.max(depth,tmp+1);
+            }
+
+        }
+        return depth;
+
     }
 }
