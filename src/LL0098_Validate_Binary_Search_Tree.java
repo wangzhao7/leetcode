d0e9fe3249ca73769b2b3bc7e1b5da921a914d7f
wangzhao <195335399@qq.com>
2020-05-26 18:45:25 +0800

    增加递归解法

--- src/LL0098_Validate_Binary_Search_Tree.java
+++ src/LL0098_Validate_Binary_Search_Tree.java
@@ -44,4 +44,36 @@ public class LL0098_Validate_Binary_Search_Tree
         }
         return true;
     }
+
+    //递归
+    public boolean isValidBST_1(TreeNode root)
+    {
+        return helper(root,null,null);
+    }
+
+    public boolean helper(TreeNode root, Integer lower, Integer upper)
+    {
+        if(root == null)
+        {
+            return true;
+        }
+        int val = root.val;
+        if(lower !=null && val<=lower)
+        {
+            return false;
+        }
+        if(upper !=null && val>=upper)
+        {
+            return false;
+        }
+        if(!helper(root.left,lower,val))
+        {
+            return false;
+        }
+        if (!helper(root.right,val,upper))
+        {
+            return false;
+        }
+        return true;
+    }
 }
